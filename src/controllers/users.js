4ecb9f81a5d8fefb4e2db526c8d63e7509d1dd49
muhammadridho <bakarudin947@gmail.com>
2022-06-30 17:03:28 +0700

    update: add validation users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,17 +1,46 @@
 const response = require('../helpers/standardResponse');
 const userModel = require('../models/users');
 
+
 exports.getAllUsers = (req, res) => {
   userModel.getAllUsers((results) => {
     return response(res, 'Message from standard response', results);
   });
 };
 
-exports.createUser = (req, res) => {
-  userModel.createUser(req.body, (results) => {
-    return response(res, 'Create user successfully', results[0]);
-  });
-};
+
+const { validationResult } = require('express-validator');
+const errorResponse = require('../helpers/errorResponse');
+exports.createUser =
+  [
+    body('email')
+      .isEmail().withMessage('Email format invalid'),
+    body('username')
+      .isLength({ min: 4 }).withMessage('Username Length minimal 4 character'),
+    (req, res) => {
+      // if (req.body.username.length < 4) {
+      //   return response(res, 'Username length must be greater than 4 character', null, 400);
+      // }
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        return response(res, 'Error occured', validation.array(), 400);
+      }
+      userModel.createUser(req.body, (err, results) => {
+        if (err) {
+          if (err.code === '23505' && err.detail.includes('email')) {
+            const eres = errorResponse('Email already exist', 'email');
+            return response(res, 'Error', eres, 400);
+          } else if (err.code === '23505' && err.detail.includes('username')) {
+            const eres = errorResponse('Email already exist', 'username');
+            return response(res, 'Error', eres, 400);
+          }
+          return response(res, 'Error', null, 400);
+        } else {
+          return response(res, 'Create user successfully', results[0]);
+        }
+      });
+    }
+  ];
 
 exports.editUser = (req, res) => {
   const { id } = req.params;
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,8 +2,17 @@ const users = require('express').Router();
 
 const userController = require('../controllers/users');
 
+const { body } = require('express-validator');
+
+const createUserValidator = [
+  body('email')
+    .isEmail().withMessage('Email format invalid'),
+  body('username')
+    .isLength({ min: 4 }).withMessage('Username length minimal 4 character')
+];
+
 users.get('/', userController.getAllUsers);
-users.post('/', userController.createUser);
+users.post('/', ...createUserValidator, userController.createUser);
 users.patch('/:id', userController.editUser);
 users.delete('/:id', userController.deleteUser);
 
